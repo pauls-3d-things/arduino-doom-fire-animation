1ca96e353daae01863b6048f8803b68ae219d675
Paul Smith <code@uvwxy.de>
2018-12-29 12:53:28 +0100

    motion

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@ Adafruit_ST7735 tft = Adafruit_ST7735(D8, D0, -1);
 
 SimplexNoise sn;
 
+float m = 0;
+
 void setup(void) {
   tft.initR(INITR_MINI160x80);
   tft.setRotation(3);
@@ -15,11 +17,13 @@ void setup(void) {
 }
 
 void loop(void) {
+  m += 2.5;
   tft.startWrite();
   tft.setAddrWindow(0, 0, 160, 80);
   for (uint8_t y = 0; y < 80; y++) {
     for (uint8_t x = 0; x < 160; x++) {
-      tft.writePixel(rgb(255 * ((sn.noise(x / 8.0, y / 8.0) + 1.0) / 2.0), 0, 0));
+      tft.writePixel(
+          rgb(255 * ((sn.noise(x / 8.0, (y + m) / 8.0) + 1.0) / 2.0), 0, 0));
     }
   }
   tft.endWrite();
