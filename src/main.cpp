3c76b9cef67b68f7a12a0e67e07dd76f3278e5b2
Paul Smith <code@uvwxy.de>
2018-12-29 12:45:36 +0100

    add noise

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 #include <Arduino.h>
+#include <SimplexNoise.h>
 
-#define rgb(r,g,b) tft.color565(b,g,r)
+#define rgb(r, g, b) tft.color565(b, g, r)
 Adafruit_ST7735 tft = Adafruit_ST7735(D8, D0, -1);
+
+SimplexNoise sn;
 
 void setup(void) {
   tft.initR(INITR_MINI160x80);
@@ -16,7 +19,7 @@ void loop(void) {
   tft.setAddrWindow(0, 0, 160, 80);
   for (uint8_t y = 0; y < 80; y++) {
     for (uint8_t x = 0; x < 160; x++) {
-      tft.writePixel(rgb(random(255), 0, 0));
+      tft.writePixel(rgb(255 * ((sn.noise(x, y) + 1.0) / 2.0), 0, 0));
     }
   }
   tft.endWrite();
